fc910c47077bda3785b71bb5899cdc7eb4cfd7b3
smaudd <smauddproject@gmail.com>
2019-05-21 19:24:35 +0200

    Counter removed

--- main/main.js
+++ main/main.js
@@ -6,21 +6,5 @@ function loadTips() {
     document.getElementById('tips').innerHTML='<object type="text/html" data="./main/tips.html"></object>';
 }
 
-function counter() {
-
-var n = localStorage.getItem('on_load_counter');
-
-if (n === null) {
-    n = 0;
-}
-
-n++;
-
-localStorage.setItem("on_load_counter", n);
-
-document.getElementById('CounterVisitor').innerHTML = n;
-}
-
 loadTips();
 loadApp();
-counter();
